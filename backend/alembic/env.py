759470c68d887a1439e540df51824eb67ce07625
fabianxdinesh@gmail.com <fabianxdinesh@gmail.com>
2026-05-08 14:54:00 +0530

    added test print cases in alembic/env.py line 12, 13 and db.session line 5

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,6 +9,9 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+from core.config import settings
+print(settings.DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- backend/database/session.py
+++ backend/database/session.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
+print(settings.DATABASE_URL)
 engine = create_engine(settings.DATABASE_URL)
 #Meaning: Prepare connection to database.
 #Note: Does NOT connect yet.
