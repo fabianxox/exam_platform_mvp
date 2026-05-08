f47e233b46bee2b6b6d00d109fa0c3dc035894d1
fabianxdinesh@gmail.com <fabianxdinesh@gmail.com>
2026-05-08 15:04:13 +0530

    line 13, 14 in alembic/env.py, as sqlalchemy.url is not being injected by render

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,6 +8,12 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from core.config import settings
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
 
 from core.config import settings
 print(settings.DATABASE_URL)
