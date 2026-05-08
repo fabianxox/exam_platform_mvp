a0685bdd33f8c70f61ee4a28b69effd05b3116c0
fabianxdinesh@gmail.com <fabianxdinesh@gmail.com>
2026-05-08 19:28:40 +0530

    updated main to acllow temp cors, fe/src/services.api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  //baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://exam-platform-mvp.onrender.com"
 })
 
 export default api
